57d05e8f0244fa8756a4df9416d8c33eeb7f5471
Nikolay Litvinenko <11arhangel05@mail.ru>
2021-06-07 23:19:49 +0300

    Update sharedStore.js

--- src/storege/sharedStore.js
+++ src/storege/sharedStore.js
@@ -1,15 +1,1 @@
-export const globalStore = {
 
-    user_login: {},
-
-    firebirdDB: {
-        url: 'http://lk.massiv-yug.ru',
-        port: ':9001',
-    },
-
-    mongoDB: {
-        url: 'http://localhost',
-        port: ':3000'
-    }
-
-};
